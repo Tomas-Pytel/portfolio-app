1010f5b4c818de0e30517d78c2a719e4f039909c
Tomas-Pytel <tomas.pytel12@gmail.com>
2026-05-18 12:50:59 +0200

    Updated color blur and some text features (#20)

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,17 +5,17 @@ import { Badge } from "@/components/ui/badge" // <-- 1. Import Badge
 export default function Hero() {
   return (
     <section className="relative flex flex-col items-center justify-center overflow-hidden px-6 py-24 text-center">
-      {/* Background Decorative Glow */}
-      <div className="absolute top-0 -z-10 h-full w-full bg-zinc-950">
-        <div className="absolute top-0 right-auto bottom-auto left-[20%] h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-amber-500/20 opacity-50 blur-[100px]"></div>
-        <div className="absolute top-0 right-[10%] bottom-auto left-auto h-[400px] w-[400px] translate-x-[20%] translate-y-[10%] rounded-full bg-orange-600/15 opacity-50 blur-[100px]"></div>
+      {/** Background blur */}
+      <div className="pointer-events-none absolute inset-0">
+        <div className="absolute bottom-0 left-1/2 h-125 w-175 -translate-x-1/2 translate-y-[40%] rounded-full bg-amber-500/20 opacity-50 blur-[100px]"></div>
+        <div className="absolute bottom-0 left-1/2 h-100 w-125 translate-x-[-30%] translate-y-[50%] rounded-full bg-orange-600/20 opacity-50 blur-[100px]"></div>
       </div>
 
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        className="flex flex-col items-center"
+        className="z-10flex relative flex-col items-center"
       >
         <Badge
           variant="outline"
@@ -25,7 +25,7 @@ export default function Hero() {
         </Badge>
 
         <h1 className="mb-6 cursor-default bg-linear-to-r from-amber-400 to-orange-500 bg-clip-text text-5xl font-bold tracking-tighter text-white transition-colors duration-500 hover:text-transparent md:text-7xl">
-          Tvorím digitálne produkty <br /> pomocou kódu a dizajnu.
+          Tvorím digitálne produkty <br /> pomocou kódu a dizajnu
         </h1>
 
         <p className="mx-auto mb-10 max-w-150 text-lg leading-relaxed text-zinc-400">
